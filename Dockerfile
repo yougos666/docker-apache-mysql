18c9b95da1a63188cf3c54dc7567f53a7c08803d
dleriche <david.leriche@openwide.fr>
2015-06-11 15:19:56 +0200

    Init repository

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:latest
+
+# Prevent some error messages
+ENV DEBIAN_FRONTEND noninteractive
+
+# Install latest updates and mysql
+RUN apt-get update;apt-get upgrade -y; apt-get -y install vim apache2 supervisor wget curl php5 php5-mysql php5-gd php5-cli php5-curl php-apc git mysql-client mysql-server php5-dev php-pear php5-dev php5-intl php5-xsl openssh-client php5-mcrypt
+
+RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+ADD start.sh /usr/local/bin/start.sh
+RUN chmod -v +x /usr/local/bin/start.sh
+
+ADD supervisord.conf /etc/supervisord.conf
+
+# Set Standard settings
+ENV user admin
+ENV password admin
+ENV right WRITE
+
+EXPOSE 80 3306
+
+CMD ["/usr/local/bin/start.sh"]
---
+++ start.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ -e /root/database_dump.lock ]
+then
+    echo  "Les dumps ont déjà été importés"
+    echo "Starting MySQL Server"
+    /usr/sbin/mysqld
+
+    tail -f /var/log/mysql/error.log
+
+else
+    touch /root/database_dump.lock
+fi
+
+
+PATH_DIR_DUMP=/root/database_dump/
+if [ -d $PATH_DIR_DUMP ]; then
+    DUMP_LIST=$(ls -A ${PATH_DIR_DUMP})      
+    for DUMP_FILE in $DUMP_LIST
+    do
+        /usr/sbin/mysqld &
+        sleep 5
+        echo "Creating database ${DUMP_FILE%%.*}"
+        echo "Create DATABASE ${DUMP_FILE%%.*}" | mysql --default-character-set=utf8
+        sleep 5
+        curl file:${PATH_DIR_DUMP}/${DUMP_FILE} | mysql -u root ${DUMP_FILE%%.*} --default-character-set=utf8
+        mysqladmin shutdown
+        echo "finished"
+    done
+else
+    rm /root/database_dump.lock;
+fi
+
+
+# Now the provided user credentials are added
+/usr/sbin/mysqld &
+sleep 5
+echo "Creating user"
+echo "CREATE USER '$user' IDENTIFIED BY '$password'" | mysql --default-character-set=utf8
+echo "REVOKE ALL PRIVILEGES ON *.* FROM '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+echo "GRANT SELECT ON *.* TO '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+echo "finished"
+
+if [ "$right" = "WRITE" ]; then
+  echo "adding write access"
+  echo "GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' identified by '$password' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+fi
+
+# And we restart the server to go operational
+mysqladmin shutdown
+#echo "Starting MySQL Server"
+#/usr/sbin/mysqld
+
+/usr/bin/supervisord -n
